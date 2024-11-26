3ea105bfd0b0bbbc628b01bc97cbf728889ad9c8
Amirabas Ahmadnia <ahmadniaamirabas@gmail.com>
2024-11-26 12:07:43 +0330

    feat: added sample pages

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('profile/', views.profile, name='profile'),
+    path('contact-us/', views.contact_us, name='contact_us'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to the Property Management Dashboard!")
+
+def about_us(request):
+    return render(request, 'dashboard/about_us.html')
+
+def profile(request):
+    return render(request, 'dashboard/profile.html')
+
+def contact_us(request):
+    return render(request, 'dashboard/contact_us.html')
