8a2b1b988914d014dfed143fc23003ab630330b7
Amirabas Ahmadnia <ahmadniaamirabas@gmail.com>
2024-12-20 01:41:07 +0330

    chore: removed all of files

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about-us/', views.about_us, name='about_us'),
-    path('profile/', views.profile, name='profile'),
-    path('contact-us/', views.contact_us, name='contact_us'),
-]
